731107b184d02df688b6e5296ffd5b5b6777ef7f
vetriselvan-pv <85449760+vetriselvan-pv@users.noreply.github.com>
2021-10-19 20:48:18 +0530

    register-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import './App.css';
+import  RegisterationForm  from "./component/registeration-form/registeration-form";
 
 function App() {
-
-  function getName(name : string){
-    alert(name)
-  }
-
-  function add(a:number){
-    alert(a+10)
-  }
-
-  function consoleName(name:string){
-    console.log(name)
-  }
-
-
-  
+ 
 
   return (
     <div>
-      <h1>New Heading</h1>
-      <h3>function compoennt added</h3>
-      <button onClick={()=> {add(1)}}>Click Me !</button>
-      <button onClick={()=> {getName('vetri')}}>Click Me !</button>
+      <RegisterationForm />
     </div>
     );
 }
