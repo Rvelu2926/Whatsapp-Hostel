210bdb69cb9d9670539052f52eb37284729dbd7c
vetriselvan-pv <85449760+vetriselvan-pv@users.noreply.github.com>
2021-10-28 20:02:04 +0530

    feat(eslint-config): eslint prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-    bracketSpacing: true,
-    jsxBracketSameLine: true,
-    singleQuote: true,
-    trailingComma: 'all',
-  };
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  semi: false,
+  useTabs: false,
+  singleQuote: true,
+  tabWidth: 2,
+}
