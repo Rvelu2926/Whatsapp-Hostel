48471a81b0de03aebb83297928c7bdadd3058dea
b-karthikraja <karthikrajab11@gmail.com>
2021-11-23 20:45:45 +0530

    feat(loginpage): this is login page used mui

--- src/pages/admin-dashboard/AdminDashboard.tsx
+++ src/pages/admin-dashboard/AdminDashboard.tsx
@@ -11,6 +11,7 @@ export default function AdminDashboard() {
       <DashboardTopBar></DashboardTopBar>
       <DashboardCard></DashboardCard>
       <DashboardList></DashboardList>
+      <DashboardList></DashboardList>
     </div>
   )
 }
